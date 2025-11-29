a25a38df7686a024bb918cf0c30fdcf243739e4a
York Xiang <bombless@126.com>
2025-11-29 10:48:08 +0800

    美化build.bat输出

--- build.bat
+++ build.bat
@@ -8,19 +8,19 @@ REM Assemble all source files
 @echo on
 @echo Assembling data.asm...
 %ML64% /c /Fo"obj\data.obj" src\data.asm
-if errorlevel 1 goto error
+@if errorlevel 1 goto error
 
 @echo Assembling callbacks.asm...
 %ML64% /c /Fo"obj\callbacks.obj" src\callbacks.asm
-if errorlevel 1 goto error
+@if errorlevel 1 goto error
 
 @echo Assembling parallel.asm...
 %ML64% /c /Fo"obj\parallel.obj" src\parallel.asm
-if errorlevel 1 goto error
+@if errorlevel 1 goto error
 
 @echo Assembling main.asm...
 %ML64% /c /Fo"obj\main.obj" src\main.asm
-if errorlevel 1 goto error
+@if errorlevel 1 goto error
 
 @REM Link all object files
 @echo Linking...
@@ -29,7 +29,7 @@ if errorlevel 1 goto error
     obj\data.obj obj\callbacks.obj obj\parallel.obj obj\main.obj ^
     /OUT:SpaceGame.efi
 
-if errorlevel 1 goto error
+@if errorlevel 1 goto error
 
 @echo Build successful!
 @goto end
